e1dbaa2b63a3c14e4a9a75fd838fa297356f8a04
Tim <th.tsai21@gmail.com>
2026-05-30 12:51:27 +0800

    Remove unused source files: data.py, model.py, plot.py, and train.py

--- src/model.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from transformers import AutoModelForSequenceClassification, PreTrainedModel
-from config import MODEL_NAME
-
-
-def get_model() -> PreTrainedModel:
-    logging.getLogger("transformers").setLevel(logging.ERROR)
-    return AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, num_labels=2)
